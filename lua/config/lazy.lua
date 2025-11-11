ec3eaa63ccb627ef220597543277bfae92599fcf
Kacper Gajda <kgajda226@gmail.com>
2025-11-11 11:48:33 +0100

    lazy.lua use default config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,6 +15,12 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
@@ -27,4 +33,3 @@ require("lazy").setup({
   -- automatically check for plugin updates
   checker = { enabled = true },
 })
-
