468c8caad2f91efdff50d3e2abb97627d6dd2061
Kacper Gajda <kgajda226@gmail.com>
2025-11-12 14:04:04 +0100

    remove comments

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,20 +1,3 @@
---[[
-return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000,
-  config = function ()
-    require("catppuccin").setup({
-      transparent_background = true,
-      float = {
-        transparent = true,
-      },
-    })
-    vim.cmd("colorscheme catppuccin")
-  end
-}
-]]--
-
 return {
 	"rose-pine/neovim",
 	name = "rose-pine",
