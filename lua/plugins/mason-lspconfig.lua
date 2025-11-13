1f542feee7f17d08f8c0c68cc996d5f790073a7b
Kacper Gajda <kgajda226@gmail.com>
2025-11-13 11:52:02 +0100

    fix vue lsp loading
    according to:
    https://github.com/vuejs/language-tools/discussions/5603
    
    Could not find 'ts_ls' or 'vtsls' lsp client, required by 'vue_ls'. #5603
    Probably the problem is that both language servers are starting in
    parallel and if vtsls takes too long vue_ls runs into that issue.

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,10 +1,19 @@
 return {
-	"mason-org/mason-lspconfig.nvim",
-	opts = {
-      ensure_installed = { "cssls", "gopls", "html", "intelephense", "lua_ls", "tailwindcss", "ts_ls", "vtsls", "vue_ls" },
-	},
-	dependencies = {
-		{ "mason-org/mason.nvim", opts = {} },
-		"neovim/nvim-lspconfig",
-	},
+  "mason-org/mason-lspconfig.nvim",
+  opts = {
+    ensure_installed = { "cssls", "gopls", "html", "intelephense", "lua_ls", "tailwindcss", "ts_ls", "vtsls", "vue_ls" },
+  },
+  dependencies = {
+    { "mason-org/mason.nvim", opts = {} },
+    "neovim/nvim-lspconfig",
+  },
+  config = function()
+    vim.lsp.config("vtsls", {
+      filetypes = { "typescript", "javascript", "vue" },
+      on_attach = function()
+        vim.lsp.enable("vue_ls")
+      end
+    })
+    vim.lsp.enable({"vtsls"})
+  end
 }
