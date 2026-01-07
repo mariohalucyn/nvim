1f88a723f0a1b2b5d1e54526a1c207901c5fb241
Kacper Gajda <kgajda226@gmail.com>
2026-01-07 14:46:53 +0100

    fix vue typescipt lsp thing

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,21 +1,57 @@
 return {
     "mason-org/mason-lspconfig.nvim",
     opts = {
-        ensure_installed = { "cssls", "eslint", "gopls", "html", "intelephense", "lua_ls", "pyright", "tailwindcss", "ts_ls", "vtsls", "vue_ls" },
+        ensure_installed = {
+            "cssls",
+            "eslint",
+            "gopls",
+            "html",
+            "intelephense",
+            "lua_ls",
+            "pyright",
+            "tailwindcss",
+            "ts_ls",
+            "vue_ls"
+        },
     },
     dependencies = {
         { "mason-org/mason.nvim", opts = {} },
         "neovim/nvim-lspconfig",
     },
-    config = function(_, opts)
-        require("mason-lspconfig").setup(opts)
-        vim.lsp.config("vtsls", {
-            filetypes = { "typescript", "javascript", "vue" },
-            on_attach = function()
-                vim.lsp.enable("vue_ls")
-            end,
+    config = function()
+        vim.lsp.config("ts_ls", {
+            init_options = {
+              plugins = {
+                {
+                  name = "@vue/typescript-plugin",
+                  location = vim.fn.stdpath "data" .. "/mason/packages/vue-language-server/node_modules/@vue/language-server",
+                  languages = { "vue" },
+                },
+              },
+            },
+            filetypes = { "typescript", "javascript",  "vue" },
         })
-
-        vim.lsp.enable({ "vtsls" })
-    end
+        -- vim.lsp.config("vtsls", {
+        --     filetypes = { "typescript", "javascript",  "vue" },
+        --     settings = {
+        --         vtsls = {
+        --             tsserver = {
+        --                 globalPlugins = {
+        --                     {
+        --                         name = "@vue/typescript-plugin",
+        --                         location = vim.fn.stdpath "data" .. "/mason/packages/vue-language-server/node_modules/@vue/language-server",
+        --                         languages = { "vue" },
+        --                         configNamespace = "typescript",
+        --                     }
+        --                 }
+        --             }
+        --         }
+        --     }
+        -- })
+        vim.lsp.enable({
+            "vue_ls",
+            "ts_ls",
+            -- "vtsls",
+        })
+    end,
 }
