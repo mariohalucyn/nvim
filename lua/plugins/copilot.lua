e8317fc3ec1ca8cfd3e50df802c8b743e0171294
Kacper Gajda <kgajda226@gmail.com>
2026-01-09 12:09:19 +0100

    fix typo

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,7 +1,7 @@
 return {
     "github/copilot.vim",
     config = function ()
-        vim.g.copilot_enabled = false
+    vim.g.copilot_enabled = false
         vim.keymap.set('n', '<leader>cd', ":Copilot disable<CR>", { silent = true })
         vim.keymap.set('n', '<leader>ce', ":Copilot enable<CR>", { silent = true })
     end
