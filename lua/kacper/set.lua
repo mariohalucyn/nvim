d2e534d4d237cebe5217895456d758fe719c92db
Kacper Gajda <kgajda226@gmail.com>
2026-01-09 11:57:16 +0100

    current line highlighting 80 char line

--- lua/kacper/set.lua
+++ lua/kacper/set.lua
@@ -1,13 +1,23 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
+
+vim.opt.cursorline = true
+
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+
 vim.opt.smartindent = true
+
 vim.opt.clipboard = "unnamedplus"
+
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+
+vim.opt.colorcolumn = "80"
 
 vim.diagnostic.config({
     virtual_text = true,
