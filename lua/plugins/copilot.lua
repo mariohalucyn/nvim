5f2d632d23e31ba2ddff50d8d67e2f51500265da
Kacper Gajda <kgajda226@gmail.com>
2026-01-09 12:05:03 +0100

    disable copilot on startup

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,7 +1,8 @@
 return {
     "github/copilot.vim",
     config = function ()
-        vim.keymap.set('n', '<leader>cd', ":Copilot disable<CR>", {noremap = true, silent = true})
-        vim.keymap.set('n', '<leader>ce', ":Copilot enable<CR>", {noremap = true, silent = true})
+        vim.g.copilot_enabled = false
+        vim.keymap.set('n', '<leader>cd', ":Copilot disable<CR>", { silent = true })
+        vim.keymap.set('n', '<leader>ce', ":Copilot enable<CR>", { silent = true })
     end
 }
